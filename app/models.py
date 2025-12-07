5a6db1072c5d96cade46fd732d536208918a7131
Esteban <eb.campana21@info.uas.edu.mx>
2025-12-06 18:04:52 -0700

    Crear pydantic model en app/models y agregar archivo de requerimientos

---
+++ app/models.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel, EmailStr
+
+class CrearLead(BaseModel):
+    name: str
+    email: EmailStr
+    phone: str
+    restaurant_type: str
+    city: str
+    lat: float | None = None
+    long: float | None = None
+    
+class RespuestaLead(CrearLead):
+    id: int
+    name: str
+    lat: float | None = None
+    long: float | None = None
